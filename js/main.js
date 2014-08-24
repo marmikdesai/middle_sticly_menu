68758ac6dde425420810c03c799863108582138e
Marmik Desai <marmikdesai@live.com>
2014-08-24 23:14:01 +0530

    remove unnecessary javascript code
    remove unnecessary javascript code

--- js/main.js
+++ js/main.js
@@ -4,21 +4,17 @@
  * Further changes, comments: @Marmik Desai
  * Licensed under the MIT license
  */
-
-if ($("body")) {
-	var $navigation = $("nav");
-	var $empty_div = $("<div class='empty_div' />", {
-		"height": $navigation.outerHeight()
-	});
-	if ($navigation.size()) {
-		$(window).scroll(function() {
-			if (!$navigation.hasClass('sticky') && $(window).scrollTop() > $navigation.offset().top) {
-				$navigation.before($empty_div);
-				$navigation.addClass("sticky");
-			} else if ($navigation.hasClass('sticky') && $(window).scrollTop() == 0) {
-				$navigation.removeClass("sticky");
-				$empty_div.remove();
-			}
-		});
+$(function(){
+	sticky_menu();
+});
+$(window).scroll(function() {
+	sticky_menu()
+});
+function sticky_menu(){
+	var $navigation = $("nav");	
+	if ($(window).scrollTop() >= $navigation.height()) {
+		$navigation.addClass("sticky");
+	} else if ($navigation.hasClass('sticky')) {
+		$navigation.removeClass("sticky");
 	}
 }
